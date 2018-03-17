9af0dfc71fc00ef8f297c8218007f3e1d08d70c7
Gerson Dias <contato@gersondias.net>
2018-03-16 22:45:07 -0300

    feat: Getting posts and categories from API
    Using fetch API in order to get items from Readable API.
    This commit uses only internal states of components and make
    calls to API directly in ComponentDidMount. This will change
    soon.

--- front/src/components/Categories/Categories.jsx
+++ front/src/components/Categories/Categories.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Categories = props => {
-	return <div>Categorie: {props.match.params.categories}</div>;
-};
+import { getAllCategories } from '../../services/ReadableAPI';
+class Categories extends React.Component {
+	state = {
+		categories: [],
+	};
+
+	componentDidMount() {
+		getAllCategories().then(data => this.setState({ categories: data }));
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Categories</h1>
+				<ul>
+					{this.state.categories.map(category => {
+						return (
+							<li key={category.path}>
+								<Link to={`/${category.path}`}>{category.name}</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
+}
 
 export { Categories };
--- front/src/components/Posts/posts.js
+++ front/src/components/Posts/posts.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getAllPosts } from '../../services/ReadableAPI';
 
 class posts extends React.Component {
-	static propTypes = {};
+  static propTypes = {};
 
-	render = () => <div>posts</div>;
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    getAllPosts().then(res => this.setState({ posts: res }));
+  }
+
+  render = () => (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {this.state.posts.map(post => {
+          return (
+            <li key={post.id}>
+              {post.title} ({post.category})
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({});
